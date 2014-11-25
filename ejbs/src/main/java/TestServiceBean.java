8decff0e0b093999c22efecf028de99b56d57e2d
tiwari.pankaj@gmail.com <tiwari.pankaj@gmail.com>
2014-11-25 16:22:35 +0530

    Domain objects some changes

---
+++ ejbs/src/main/java/TestServiceBean.java
@@ -0,0 +1,10 @@
+import javax.ejb.Stateless;
+
+/**
+ * Created by pankaj on 23-11-2014.
+ */
+@Stateless(name = "TestServiceEJB")
+public class TestServiceBean {
+    public TestServiceBean() {
+    }
+}
